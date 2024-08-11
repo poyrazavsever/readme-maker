ebd026ecb5161ac877e81e634d99998bddeb02ca
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-11 18:37:28 +0300

    feat: conditional section rendering completed

--- pages/readmemaker.js
+++ pages/readmemaker.js
@@ -2,15 +2,23 @@ import React, {useState} from 'react'
 
 // Components
 import TopSection from '@/components/ReadmeMaker/TopSection'
-
+import ProfileGenerator from '@/components/ReadmeMaker/ProfileGenerator'
+import ProjectGenerator from '@/components/ReadmeMaker/ProjectGenerator'
 
 function ReadmeMaker() {
 
-    const [section, setSection] = useState("project")
+    const [section, setSection] = useState(false)
 
     return (
         <div className='md:container md:mx-auto'>
             <TopSection section={section} setSection={setSection}/>
+
+            {!section && 
+                <ProfileGenerator />
+            }
+            {section && 
+                <ProjectGenerator />
+            }
         </div>
     )
 }
