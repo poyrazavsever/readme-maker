4e53ae7fa7fbb1a6b70058b7d2bab44d8e5cfbe7
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-12 23:06:18 +0300

    tested

--- components/ReadmeMaker/ProfileCustomization/Templates.jsx
+++ components/ReadmeMaker/ProfileCustomization/Templates.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { promises as fs } from 'fs';
 
 function Templates() {
 
-  const templateData = fs.watch.
-
   return (
     <div>
-      <div className=''></div>
+      <div className=''>{file}</div>
     </div>
   )
 }
