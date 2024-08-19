fafd260998e880c4e7782b08e11c78e79f8e758f
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-19 10:38:38 +0300

    feat: layout deleted & i redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import "@/styles/globals.css";
-import Layout from "@/Layout/layout";
 
 export default function App({ Component, pageProps }) {
   return(
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Component {...pageProps} />
   );
 }
