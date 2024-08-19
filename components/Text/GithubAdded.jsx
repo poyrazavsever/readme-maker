7b5fc89e8c3fc20e7fe6bc254a2a7baf5b5c75a1
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-19 16:16:34 +0300

    feat: githubAdded done

---
+++ components/Text/GithubAdded.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setGithubUsername } from "@/redux/template/textSlice";
+import toast from 'react-hot-toast'
+
+// Icon
+import { IoAddCircleOutline } from "react-icons/io5";
+
+function GithubAdded() {
+
+    const dispatch = useDispatch()
+
+    const [githubChange, setGithubChange] = useState("")
+
+    const onSave = [githubChange].every(Boolean)
+
+    const handleClick = () => {
+
+        let response = fetch(`https://api.github.com/users/${githubChange}`)
+
+        if (!response) {
+            toast.warning("Malasef kullanıcı adı doğru değil.")
+        }
+
+        if (onSave && response) {
+            dispatch(setGithubUsername(githubChange))
+        }
+    }
+
+    return (
+
+        <div className="w-64 flex flex-col items-start gap-4">
+
+            <div className="flex flex-col items-start gap-3 w-full">
+                <label htmlFor="yourName" className="text-sm uppercase font-semibold tracking-wider text-slate-200"> Github Username</label>
+                <input
+                    type="text"
+                    id="yourName"
+                    value={githubChange}
+                    placeholder="David_Jhonsan"
+                    onChange={(e) => setGithubChange(e.target.value)}
+                    className="focus:outline-none py-2 px-4 rounded bg-slate-700 text-slate-100 w-full placeholder:text-slate-500"
+                />
+            </div>
+
+            <button
+                className="flex items-center justify-center gap-2 py-2 bg-slate-700 w-full rounded transition-all disabled:hover:bg-red-700 hover:bg-green-800"
+                onClick={handleClick}
+                disabled={!onSave}
+            >
+                <IoAddCircleOutline className="text-slate-100 text-lg" />
+                <span className="uppercase font-semibold text-slate-200">Add</span>
+            </button>
+
+        </div>
+    )
+}
+
+export default GithubAdded
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import TemplateSelector from '../components/TemplateSelector';
+import { Toaster } from 'react-hot-toast';
 
-import NameAdded from '@/components/Text/NameAdded';
+import GithubAdded from '@/components/Text/GithubAdded';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-950 p-4">
 
+      <Toaster />
+
       <div className='m-8'>
         <TemplateSelector />
       </div>
 
-      <NameAdded />
+      <GithubAdded />
 
     </div>
   );
